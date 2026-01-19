2fadc14b117975d857c444a4bb07c62ba4093ce8
crousty24-bit <allen.kch24@gmail.com>
2026-01-19 11:16:08 +0100

    add func to get factorial w/ user number using while loop

---
+++ script_2.js
@@ -0,0 +1,10 @@
+const number = prompt("Give me a number");
+function fact(n) {
+  let result = 1;
+  while (n > 1) { //type coercion : auto & implicit conversion of values (str to num)
+    result *= n; //assignement + operator same as: result = result * n
+    n--;
+  }
+  return result;
+}
+console.log(fact(number))
