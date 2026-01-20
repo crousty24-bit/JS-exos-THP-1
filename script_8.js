c90cbe6e51c91206bbc99b5e8406c8fabec4bf44
crousty24-bit <allen.kch24@gmail.com>
2026-01-20 20:03:28 +0100

    add func to get data answers using map filter reduce

--- script_8.js
+++ script_8.js
@@ -1,1 +1,26 @@
 import {users} from "./module/users.js"
+
+console.log("What's the average revenue per users ?")
+let average = users.reduce((sum, current) => sum + current.revenue, 0) / users.length;
+console.log(`${Math.round(average)} is the average revenue`)
+
+let allRevenue = users.map(function(n){
+  return `${n.firstName} ` + `${n.lastName} :` + ` ${n.revenue}`
+});
+//console.log(allRevenue)
+
+console.log("What's the percentage of users earning revenue (> 0) ?")
+let revenue = users.filter(function(e){
+  return (e.revenue > 0)
+});
+let percent = Math.round((revenue.length / users.length) * 100);
+console.log(`${percent} %`)
+
+console.log("What's the average revenue of those users ?")
+let avgRevenue = revenue.reduce((sum, current) => sum + current.revenue, 0) / revenue.length;
+console.log(`${Math.round(avgRevenue)} is the average revenue`)
+
+let list = revenue.map(function(n){
+  return `${n.firstName} ` + `${n.lastName} :` + ` ${n.revenue}`
+});
+//console.log(list)
